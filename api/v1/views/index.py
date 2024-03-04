af62e44409d9cf08208413b74ac9fce3332e8e46
Victor Jonathan <136059175+MrVeeJon@users.noreply.github.com>
2024-03-04 13:29:35 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+Flask route which returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
@@ -10,7 +10,7 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def status():
     """
-    function for status route that returns the status
+    function for status route which returns the status
     """
     if request.method == 'GET':
         resp = {"status": "OK"}
@@ -20,7 +20,7 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """
-    function to return the count of all class objects
+    function that'll return the count of all class objects
     """
     if request.method == 'GET':
         response = {}
